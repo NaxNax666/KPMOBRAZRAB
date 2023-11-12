64d0855d0a21493720f7474b399f9d8117555509
A.Galushkin <A.Galushkin@avilex.ru>
2023-11-12 18:54:07 +0300

    Finally worked fragments

--- app/src/main/java/com/example/kpmobrazrab/MainActivity.kt
+++ app/src/main/java/com/example/kpmobrazrab/MainActivity.kt
@@ -1,46 +1,31 @@
 package com.example.kpmobrazrab
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.kpmobrazrab.ui.theme.KPMOBRAZRABTheme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.kpmobrazrab.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var fragmentManager: FragmentManager
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            KPMOBRAZRABTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonFragment1.setOnClickListener {
+            goToFragment(Fragment1())
+
+        }
+        binding.buttonFragment2.setOnClickListener {
+            goToFragment(Fragment2())
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KPMOBRAZRABTheme {
-        Greeting("Android")
+    private fun goToFragment(fragment: Fragment){
+        fragmentManager = supportFragmentManager
+        fragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragment).commit()
     }
 }
