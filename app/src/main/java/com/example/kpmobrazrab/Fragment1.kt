ea6aeb44f7c1f4a69a67af5edb336ae3b11cd29b
A.Galushkin <A.Galushkin@avilex.ru>
2023-11-14 22:54:21 +0300

    worked ChangeBW

--- app/src/main/java/com/example/kpmobrazrab/Fragment1.kt
+++ app/src/main/java/com/example/kpmobrazrab/Fragment1.kt
@@ -1,21 +1,36 @@
 package com.example.kpmobrazrab
 
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.kpmobrazrab.databinding.Fragment1Binding
+
+
 
 
 class Fragment1 : Fragment() {
 
-
+    lateinit var binding: Fragment1Binding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_1, container, false)
+        binding = Fragment1Binding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.imageView.setImageResource(R.drawable.img1)
+        binding.executeButton.setOnClickListener {
+
+            val bitmap = (binding.imageView.getDrawable() as BitmapDrawable).bitmap
+
+            binding.imageView2.setImageBitmap(CoreSystem.ConvertToBlackWhite(bitmap))
+        }
     }
 
 }
