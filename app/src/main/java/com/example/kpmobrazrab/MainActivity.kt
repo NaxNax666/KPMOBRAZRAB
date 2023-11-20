0b752bd761976c44b280a655a9b7292f6bdbe479
A.Galushkin <A.Galushkin@avilex.ru>
2023-11-20 18:51:01 +0300

    backbutton

--- app/src/main/java/com/example/kpmobrazrab/MainActivity.kt
+++ app/src/main/java/com/example/kpmobrazrab/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.kpmobrazrab
 
 import MAIN
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.FragmentManager
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.kpmobrazrab.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var fragmentManager: FragmentManager
     private lateinit var binding: ActivityMainBinding
@@ -17,11 +21,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
+
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
         navController = Navigation.findNavController(this, R.id.fragmentContainer)
         MAIN = this
-
-
+        binding.navView.setNavigationItemSelectedListener(this)
+        val toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.openNav,R.string.closeNav)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_home -> MAIN.navController.navigate(R.id.mainFragment)
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if(binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        }else{
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
 
 }
